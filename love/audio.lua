8aa2a5564e95179f607c0c1086c76d441e894174
theMachienHandler <themachinehandler@gmail.com>
2025-05-05 16:49:39 -0300

    Brings all global declarations to local scope

--- love/audio.lua
+++ love/audio.lua
@@ -1,18 +1,19 @@
-AudioFilePriority = {}
+local AudioFilePriority = {}
 
 function love.audio.newSource(filename, type)
+    local Source = require("locals/Source")
     -- will open an audio file using LPPVita
     Logger.logfile("audio.newSource - file path: " .. GAME_PATH .. filename)
-    local sound_source = Source:new(filename, 0)
-    Logger.logfile("audio.newSource - type: " .. type)
+    local sound_source = Source(filename, 0)
+    Logger.logfile("audio.newSource - type: " .. type .. " - not being used when called")
     --Logger.logfile("audio.newSource - sound_id (LPPVita): " .. tostring(snd))
     --Logger.logfile("audio.newSource - sound source: " .. Logger.tprint(snd_source))
-    Logger.logfile("audio.newSource - \nAudioFilePriority:" .. Logger.tprint(AudioFilePriority))
-    Logger.logfile("audio.newSource - wip - passed")
+    Logger.logfile("audio.newSource - \nAudioFilePriority: " .. Logger.tprint(AudioFilePriority) .. " - not being used right now")
+    Logger.logfile("audio.newSource - wip - fake - passed")
 
     return sound_source
 end
 
 function love.audio.play(sound)
-    -- implement
+    -- implement - if needed
 end
